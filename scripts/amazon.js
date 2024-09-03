cf99a6411f5881f1189665ca8e0aadaa3e1ea9fd
Anshika2520 <anshikamishra693@gmail.com>
2024-09-03 16:43:33 +0530

    Make the cart quantity interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -82,8 +82,15 @@ document.querySelectorAll('.js-add-to-cart')
       })
       }
 
+      //Now we are finding the total cartQuantity
 
-     
-      console.log(cart);
+      let cartQuantity = 0;
+      cart.forEach((item) => {
+        cartQuantity += item.quantity;
+      });
+
+      document.querySelector('.js-cart-quantity')
+        .innerHTML = cartQuantity;
+      
     });
   });
