1608a7b713a3022a393db6724e8249557ed45dbe
Anshika2520 <anshikamishra693@gmail.com>
2024-09-19 20:29:47 +0530

    create automated tests for addToCart

--- tests-jasmine/utils/moneyTest.js
+++ tests-jasmine/utils/moneyTest.js
@@ -1,4 +1,4 @@
-import { formatCurrency } from '../scripts/utils/money.js';
+import { formatCurrency } from '../../scripts/utils/money.js';
 
 //its a syntax of jasmine
 describe('test suite : formatCurrency', () => {
